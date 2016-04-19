2d58d6a971a1b087ac52d2a3e15535c2c9243390
Riddhish Bhalodia <Riddhish@Riddhishs-MacBook-Pro.local>
2016-04-19 22:53:44 +0530

    Edgelinking done

--- main.m
+++ main.m
@@ -1,3 +1,5 @@
+close all; clear all; clc;
+
 addpath('edison');
 addpath('artist_data');
 addpath('edge_maps');
@@ -7,14 +9,20 @@ tic;
 %% detecting edges using EDISON edge detect
 edges = detect_edges('artist_data/A16983.ppm');
 % imshow(edges);
+% edges = im2double(edges);
 edges = im2double(edges(1:50,1:50));
+
 %% Edge Refinement
 % 1. Edge Linking 
 [edgelist, labelededgeim] = edgelink(edges, 10);
 modify_img = zeros(size(edges));
 for i = 1:length(edgelist)
-    modify_img(edgelist{i}(1),edgelist{i}(2)) = 1;
+    for j = 1:size(edgelist{i},1)
+        modify_img(edgelist{i}(j,1),edgelist{i}(j,2)) = 1;
+    end    
 end
 figure ;
-imshowpair(edges, modify_img,'montage');
+imshowpair(edges, modify_img,'montage','scaling','none');
+figure ;
+imshow(abs(edges - modify_img));
 toc;
