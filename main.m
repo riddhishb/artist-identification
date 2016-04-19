9ad1b2855e4904dcb0bd9889de64d9faacf9783e
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2016-04-19 22:24:39 +0530

    Added edgelink

--- main.m
+++ main.m
@@ -1,6 +1,9 @@
 addpath('edison');
 addpath('artist_data');
 addpath('edge_maps');
+addpath('edge_link');
 
 edges = detect_edges('artist_data/A16983.ppm');
-imshow(edges);
+% imshow(edges);
+
+[edgelist, labelededgeim] = edgelink(edges, 10);
