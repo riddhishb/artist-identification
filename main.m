093118076c4b046a7e77655a9c06370066b31b4f
Riddhish Bhalodia <Riddhish@Riddhishs-MacBook-Pro.local>
2016-05-04 03:06:48 +0530

    Update train classifier

--- main.m
+++ main.m
@@ -10,7 +10,37 @@ addpath('classification');
 tic;
 
 im_orig = 'artist_data/original.ppm';
-im_forge = 'artist_data/forged.ppm';
+im_forge = 'artist_data/forgery.ppm';
+
+features_orig = extract_feature(im_orig);
+[num_points_orig, ~] = size(features_orig);
+rand50_orig = randperm(num_points_orig,floor(num_points_orig/2));
+features_rand50_orig = features_orig(rand50_orig,:);
+rand50_other_orig = zeros(num_points_orig - length(rand50_orig),1);
+count = 1;
+for i = 1:num_points_orig
+    if(find(rand50_orig==i))
+    else
+        rand50_other_orig(count) = i;
+        count= count + 1;
+    end 
+end    
+labels_orig = ones(num_points_orig, 1);
+
+features_forge = extract_feature(im_forge);
+[num_points_forge, ~] = size(features_forge);
+rand50_forge = randperm(num_points_forge,floor(num_points_forge/2));
+features_rand50_forge = features_forge(rand50_forge,:);
+rand50_other_forge = zeros(num_points_forge - length(rand50_forge),1);
+count=1;
+for i = 1:num_points_forge
+    if(find(rand50_forge==i))
+    else
+        rand50_other_forge(count) = i;
+        count= count + 1;
+    end 
+end 
+labels_forge = zeros(num_points_forge, 1);
 
 classifier = train_classifier(im_orig, im_forge);
 
